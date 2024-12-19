0a7d9a54b246652ef7016c42af60d1cb23cb17c6
NGAMCODE <ngamcode@gmail.com>
2024-12-19 23:21:48 +0100

    Prepare base model added

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
 
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< \n\n")
-    except Exception as e:
-        logger.exception(e)
-        raise e
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< \n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare base model stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< \n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
-from cnnClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
+from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
 
 
 class ConfigurationManager:
@@ -11,7 +11,7 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
     
-
+    # get data ingestion config
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -25,5 +25,23 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    # get base model config and params
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learing_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
 
 
